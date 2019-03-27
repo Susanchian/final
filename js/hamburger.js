c78dd235753eec38f4ce98e0fef534187f680e0c
Susan <lee21516@gmail.com>
2019-03-28 01:38:52 +0800

    fix reload problem

--- js/hamburger.js
+++ js/hamburger.js
@@ -3,10 +3,6 @@
 //漢堡選單
 $(document).ready(function(){
 	
-	$(window).resize(function(){
-		location.reload();
-	});//偵測螢幕現在的寬度，去對應css
-	
 	$(".menu").click(function(){
 		
 		$("nav").slideToggle();
